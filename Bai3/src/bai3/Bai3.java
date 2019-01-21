239873c4d38d5015f305c14d94e00084c1aea0f5
unknown <333toan@gmail.com>
2019-01-21 14:22:40 +0700

    first commit

---
+++ Bai3/src/bai3/Bai3.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pc Of Huy Toan
+ */
+public class Bai3 {
+    public static void main(String[] args) {
+        int a=100000;
+        int b=200000;
+        int c=500000;
+        for (int i = 0; i <= 3000000/a; i++) {
+            for (int j = 0; j <= 3000000/b; j++) {
+                for (int k = 0; k <= 3000000/c; k++) {
+                    if(i*a+j*b+k*c==3000000){
+                        System.out.println("3000000 đổi thành : "+i+" tờ "+a+" , "+j+" tờ "+b+" , "+k+" tờ "+c);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Bai3/src/bai3/Bai4a.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pc Of Huy Toan
+ */
+public class Bai4a {
+    public static void main(String[] args) {
+        int h;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Nhập đọ cao h = ");
+        h=input.nextInt();
+        for(int i=0;i<h;i++){
+            for(int j=0;j<2*h+1;j++){
+                if(h-i<=j&&j<=h+i){
+                    System.out.print(" * ");
+                }
+                else{
+                    System.out.print("   ");
+                }
+                
+            }
+            System.out.println("");
+        }
+    }
+}
